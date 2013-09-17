0bc7b932e2bde0820298d039f75d6a4fe70b7595
zkalev <zhivko_kalev@abv.bg>
2013-09-17 03:21:48 +0300

    .classpath deleted

--- LandmarksFinder/gen/bg/tusofia/fktt/zkalev/dessertation/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package bg.tusofia.fktt.zkalev.dessertation;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
